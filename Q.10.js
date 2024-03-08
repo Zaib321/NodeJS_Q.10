26ab650773b6605c68728b8f70d54164fa45578e
Zaib321 <161988392+Zaib321@users.noreply.github.com>
2024-03-08 05:37:52 +0500

    NodeJS_Q.10

---
+++ Q.10.js
@@ -0,0 +1,8 @@
+"use strict";
+//10 .Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+Object.defineProperty(exports, "__esModule", { value: true });
+// Author: [Muhammad Jehanzaib]
+// Dated: [Friday ferbuary 08 2024]
+// task/9 Printing my favorite num by this Program.
+let favoritenumber = 4;
+console.log(`"My favorite num is ${favoritenumber}."`);
---
+++ Q.10.ts
@@ -0,0 +1,10 @@
+//10 .Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+
+// Author: [Muhammad Jehanzaib]
+// Dated: [Friday ferbuary 08 2024]
+
+
+// task/9 Printing my favorite num by this Program.
+
+let favoritenumber: number= 4;
+console.log(`"My favorite num is ${favoritenumber}."`);
